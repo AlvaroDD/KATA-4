9ae7270fbcb12045a89ad489c33f1e620f67fb24
-Alvaro- <-Alvaro-@Alvaro>
2016-01-10 13:38:45 +0000

    kata 4

---
+++ src/KATA4/Kata4.java
@@ -0,0 +1,14 @@
+package kata4;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Kata4 {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String name = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\KATA4\\data\\emailsfilev1.txt";
+        ArrayList<String> mailList = MailListReader.read(name);
+        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
+        new HistogramDisplay(histogram).execute();
+    }
+}
